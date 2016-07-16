2260d5f51fc174f83e283fef9e3d7f441b094eea
Uday Krishna <uday.krishna@stayzilla.com>
2016-07-16 07:59:13 +0530

    Uday : ards 10

--- parseAndStore.js
+++ parseAndStore.js
@@ -10,7 +10,7 @@ var query = aql.query;
 var bindVars = aql.bindVars;
 //var db = arangojs()();
 
-var graph = db.graph('ards10');
+var graph = db.graph('ards9');
 
 graph.create({
  edgeDefinitions: [
